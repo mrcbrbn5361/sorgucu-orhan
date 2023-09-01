0071b249ba21083ca3e4396dd4b49e1120f30bf6
mrcbrbn5361 <51040879+mrcbrbn5361@users.noreply.github.com>
2023-09-01 18:23:54 +0300

    Update messageCreate.js

--- Bots/self_tracking/_events/Stats/messageCreate.js
+++ Bots/self_tracking/_events/Stats/messageCreate.js
@@ -1,42 +1,52 @@
+name: "messageCreate",
 
-const { Message } = require('discord.js-selfbot-v13');
-let { Stats, Seens } = require('../../../../Databases/Tracking');
-module.exports = {
+/**
+ * @param {Message} message 
+ * @returns {Promise<void>}
+ */
 
-    name: "messageCreate",
-
-    /**
-     * @param {Message} message 
-     * @returns {Promise<void>}
-     */
+onLoad: async function (message) {
+    if (message.author.bot || !message.guild || message.webhookID || message.channel.type === "dm") return;
     
-    onLoad: async function (message) {
-        if (message.author.bot || !message.guild || message.webhookID || message.channel.type === "dm" || client.prefix.some(x => message.content.startsWith(x))) return;
-        
-        await Seens.updateOne({userID: message.author.id}, {
-            $set: {
-                guildID: message.guildId,
-                lastSeenMessage: Date.now(),
-                lastSeen: Date.now(),
-                messageChannelId: message.channelId,
-                messageContent: message.content,
-                lastType: "MESSAGE",
-            }, 
-            $push: {
-                lastContent: {
-                    date: Date.now(),
-                    content: message.content,
-                    guild: message.guildId,
-                    channel: message.channelId,
-                }
-            }
-        }, {upsert: true});
-        
-        await Stats.updateOne({guildID: message.guildId, userID: message.author.id}, {$inc: {
-            topMessage: 1
-            }}, {upsert: true});
-
+    const seenData = await Seens.findOne({userID: message.author.id});
+    if (!seenData) {
+        seenData = {
+            userID: message.author.id,
+            guildID: message.guildId,
+            lastSeenMessage: Date.now(),
+            lastSeen: Date.now(),
+            messageChannelId: message.channelId,
+            messageContent: message.content,
+            lastType: "MESSAGE",
+            lastContent: [],
+        };
     }
+    
+    seenData.lastSeenMessage = Date.now();
+    seenData.lastSeen = Date.now();
+    seenData.messageChannelId = message.channelId;
+    seenData.messageContent = message.content;
+    seenData.lastType = "MESSAGE";
+    seenData.lastContent.push({
+        date: Date.now(),
+        content: message.content,
+        guild: message.guildId,
+        channel: message.channelId,
+    });
+    
+    await Seens.updateOne({userID: message.author.id}, seenData);
+    
+    const statsData = await Stats.findOne({guildID: message.guildId, userID: message.author.id});
+    if (!statsData) {
+        statsData = {
+            guildID: message.guildId,
+            userID: message.author.id,
+            topMessage: 0,
+        };
+    }
+    
+    statsData.topMessage++;
+    
+    await Stats.updateOne({guildID: message.guildId, userID: message.author.id}, statsData);
+
 }
-
-
